64fe6110be52fe1e1ff837a432e79c895adfea1f
MinhPhucNguyen <2051063667@e.tlu.edu.vn>
2023-05-05 22:32:05 +0700

    up code setup

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <title>Car Management</title>
+</head>
+
+<body>
+    @yield('content')
+
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.css') }}"></script>
+    <script src="{{ asset('assets/js/jquery-3.6.4.min.js') }}"></script>
+</body>
+
+</html>
